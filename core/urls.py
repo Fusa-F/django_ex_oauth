5cde969c20327220bc9f8c4a1297769235562b42
Fusa-F <cist.b218.f.fukushi@gmail.com>
2022-03-01 15:47:43 +0900

    [add]ユーザ認証/ビューの認可を作成/演習を完了

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
-from .views import listing, view_blog
+from .views import *
 
 urlpatterns = [
     path('', listing, name='listing'),
     path('view_blog/<int:blog_id>/', view_blog, name='view_blog'),
+    path('see_request', see_request, name='see_request'),
+    path('user_info', user_info, name='user_info'),
+    path('private_place', private_place, name='private_place'),
+    path('staff_place', staff_place, name='staff_place'),
+    path('add_messages', add_messages, name='add_messages'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import Blog
 
 # Create your views here.
@@ -19,3 +21,47 @@ def view_blog(request, blog_id):
         'blog': blog
     }
     return render(request, 'view_blog.html', context)
+
+# リクエストを表示する
+def see_request(request):
+    text = f"""
+        Some attributes of the HttpRequest object:
+
+        scheme: {request.scheme}
+        path:   {request.path}
+        method: {request.method}
+        GET:    {request.GET}
+        user:   {request.user}
+    """
+
+    return HttpResponse(text, content_type="text/plain")
+
+def user_info(request):
+    text = f"""
+        Selected HttpRequest.user attributes:
+
+        username:     {request.user.username}
+        is_anonymous: {request.user.is_anonymous}
+        is_staff:     {request.user.is_staff}
+        is_superuser: {request.user.is_superuser}
+        is_active:    {request.user.is_active}
+    """
+
+    return HttpResponse(text, content_type="text/plain")
+
+# ログインが必要
+@login_required
+def private_place(request):
+    return HttpResponse("Shhh, members only!", content_type="text/plain")
+
+@login_required
+def add_messages(request):
+    username = request.user.username
+    messages.add_message(request, messages.INFO, f"Hello {username}")
+    messages.add_message(request, messages.WARNING, "DANGER WILL ROBINSON")
+
+    return HttpResponse("Messages added", content_type="text/plain")
+
+@user_passes_test(lambda user: user.is_staff)
+def staff_place(request):
+    return HttpResponse("Employees must wash hands", content_type="text/plain")
